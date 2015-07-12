46b33c301ded576f3addba89e5d19853f9403b11
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-12 19:29:57 +0900

    Do not use pegjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,2 @@
 var gulp = require("gulp");
 var shell = require("gulp-shell");
-
-var PEGJS_SRC = "./src/compiler/parser.pegjs"
-
-gulp.task("pegjs", shell.task([
-  `pegjs ${PEGJS_SRC}`
-]));
-
-gulp.task("watch", ["pegjs"], function () {
-  gulp.watch(PEGJS_SRC, ["pegjs"]);
-});
-
-gulp.task("default", ["watch"]);
