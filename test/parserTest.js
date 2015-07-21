f02d631f852c70c1be2a39f870260f87375f26c7
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-21 23:20:53 +0900

    Use destruction

--- test/parserTest.js
+++ test/parserTest.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const {assert} = require("chai");
-const parser = require("../src/compiler/parser");
+const {parse, SyntaxError} = require("../src/compiler/parser");
 const emit = require("../src/compiler/emit");
 
 const testFile = fs.readFileSync(path.join(__dirname, "fixtures/TodoList.piece"), {encoding: "utf8"});
@@ -9,12 +9,12 @@ const testFile = fs.readFileSync(path.join(__dirname, "fixtures/TodoList.piece")
 describe("parse", () => {
   it("parses", () => {
     try {
-      const parsed = parser.parse(testFile);
+      const parsed = parse(testFile);
       console.log(JSON.stringify(parsed, null, 2));
       console.log(emit(parsed));
       assert(typeof parsed === "object");
     } catch (e) {
-      if (e instanceof parser.SyntaxError) {
+      if (e instanceof SyntaxError) {
         const {line, column} = e.location.start;
         throw new Error(`Syntax error at ${line}:${column}: ${e.message}`);
       } else {
