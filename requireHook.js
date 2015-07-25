a6e8d00d1dd606cb416892c5acf0d5971228afe8
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-25 10:37:02 +0900

    Use require.extensions for pegjs

--- requireHook.js
+++ requireHook.js
@@ -1,1 +1,10 @@
-require('babel/register')();
+var fs = require("fs");
+var pegjs = require("pegjs");
+
+require.extensions[".pegjs"] = function (module, filename) {
+  var source = fs.readFileSync(filename, "utf8");
+  var compiled = pegjs.buildParser(source, {output: "source"});
+  return module._compile("module.exports = " + compiled, filename);
+}
+
+require("babel/register")();
