9ac8208b23ff181c7bbe9f9ab79bf1c83da9a6d4
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-25 10:37:32 +0900

    Remove watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,5 @@ var shell = require("gulp-shell");
 var PEGJS_SRC = "./src/compiler/parser.pegjs"
 
 gulp.task("pegjs", shell.task([
-  `pegjs --trace ${PEGJS_SRC}`
+  `pegjs ${PEGJS_SRC}`
 ]));
-
-gulp.task("watch", ["pegjs"], function () {
-  gulp.watch(PEGJS_SRC, ["pegjs"]);
-});
-
-gulp.task("default", ["watch"]);
