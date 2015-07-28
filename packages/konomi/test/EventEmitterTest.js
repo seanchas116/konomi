6ba50dc3e206698c9d4d5f91e6d2a099ae1a9910
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-29 00:00:41 +0900

    Improve EventEmitter#on test

--- packages/konomi/test/EventEmitterTest.js
+++ packages/konomi/test/EventEmitterTest.js
@@ -2,16 +2,30 @@ import assert from "power-assert";
 import EventEmitter from "../src/EventEmitter";
 
 describe("EventEmitter", () => {
-  describe("#on", () => {
-    it("adds an event listener and return disposable", () => {
-      const emitter = new EventEmitter();
-      let received;
-      const subscription = emitter.on("hoge", (...args) => {
-        received = args;
-      });
-      emitter.emit("hoge", 1, 2);
-      assert.deepEqual(received, [1,2]);
 
+  describe("#on", () => {
+    const emitter = new EventEmitter();
+    let received;
+
+    const onReceive = (...args) => {
+      received = args;
+    };
+    const doNothing = () => {};
+
+    const subscription = emitter.on("hoge", onReceive);
+    emitter.on(doNothing);
+
+    emitter.emit("hoge", 1, 2);
+
+    it("adds listener and return disposable", () => {
+      assert.deepEqual(received, [1,2]);
+    });
+
+    it("adds listener at the end of listeners", () => {
+      assert(emitter.listeners("hoge").indexOf(onReceive) === 0);
+    });
+
+    it("return disposable", () => {
       subscription.dispose();
       emitter.emit("hoge", 3, 4);
       assert.deepEqual(received, [1,2]);
