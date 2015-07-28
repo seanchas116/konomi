a08c440b6b20994735c3c7a3cb10c8543254229e
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-28 23:54:53 +0900

    Test binding disposal

--- packages/konomi/test/propertyTest.js
+++ packages/konomi/test/propertyTest.js
@@ -4,14 +4,24 @@ import * as property from "../src/property";
 
 describe("property", () => {
   describe(".bind", () => {
-    it("binds a property", () => {
-      const c1 = new Component();
-      property.define(c1, "p1");
-      const c2 = new Component();
-      property.bind(c2, "p2", [[c1, "p1"]], () => c1.p1 * 2);
+    const c1 = new Component();
+    property.define(c1, "p1");
+    const c2 = new Component();
+    property.bind(c2, "p2", [[c1, "p1"]], () => c1.p1 * 2);
 
+    it("binds a property", () => {
       c1.p1 = 123;
       assert(c2.p2 === 246);
+    });
+
+    it("discard binding on source component disposal", () => {
+      c1.dispose();
+      assert(c1.changes.listeners("p1").length === 0);
+    });
+
+    it("discard binding on dest component disposal", () => {
+      c2.dispose();
+      assert(c1.changes.listeners("p1").length === 0);
     });
   });
 });
